1376fc26d6533c0fd781d54e1f09af75c276cea9
Poom Yimyuean <lebrancconvas@gmail.com>
2021-06-08 11:12:46 +0700

    :tada: Project Init.

---
+++ precache-manifest.8e1a55aedf5035ec3d08d68808871fe7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9f7b91884ed9efeae7",
+    "url": "/css/app.9e60970e.css"
+  },
+  {
+    "revision": "7f8d09870065a5b2734b0aaea8a07ad4",
+    "url": "/img/discord-logo.7f8d0987.png"
+  },
+  {
+    "revision": "e4700f800a509a5cb70358ce98044b85",
+    "url": "/img/facebook-logo.e4700f80.png"
+  },
+  {
+    "revision": "ec3a60c8c6539a07eb70b52f6737ea6e",
+    "url": "/img/github-logo.ec3a60c8.png"
+  },
+  {
+    "revision": "61869e2a1c17ca12270aba2367a12c20",
+    "url": "/img/gmail-logo.61869e2a.png"
+  },
+  {
+    "revision": "6605daad9584739ff9b9da95e9ffde66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d9f7b91884ed9efeae7",
+    "url": "/js/app.c925b417.js"
+  },
+  {
+    "revision": "e0e1b0d365c8c505e239",
+    "url": "/js/chunk-vendors.c0d513aa.js"
+  },
+  {
+    "revision": "7e280e79c0a44f0b5b3a986bba24793b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
